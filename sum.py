8a75932e141bacb876f66b57b82c994f478fd454
Mystic-Mish <mumiaktech@gmail.com>
2023-04-05 18:41:20 +0300

    Second Commit

--- sum.py
+++ sum.py
@@ -1,7 +1,7 @@
 import numbers
 
 
-def number_sqrt(lst):
+def number_sum(lst):
     def get_numbers(l):
         for e in l:
             if isinstance(e, numbers.Number):
@@ -9,11 +9,9 @@ def number_sqrt(lst):
             elif isinstance(e, (tuple, list)):
                 yield from get_numbers(e)
 
-    return sqrt(get_numbers(lst))
+    return sum(get_numbers(lst))
 
 
 x1 = [1, 3, 5, 7, 9]
-x2 = ["abc", [3, 5], 3.5, 2 + 3j, 4.5, 10]
 
-print(number_sqrt(x1))
-print(number_sqrt(x2))
+print(number_sum(x1))
